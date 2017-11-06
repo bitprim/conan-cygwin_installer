4672918e9bff9960d094a593d32b091d190539f2
SSE4 <tomskside@gmail.com>
2017-11-06 18:56:28 +0700

    - remove condition - always apply attribute

--- conanfile.py
+++ conanfile.py
@@ -65,9 +65,9 @@ class CygwinInstallerConan(ConanFile):
                         signature = f.read(10).decode()
                         if signature == '!<symlink>':
                             attributes = ctypes.windll.kernel32.GetFileAttributesW(filename)
-                            if FILE_ATTRIBUTE_SYSTEM != (attributes & FILE_ATTRIBUTE_SYSTEM):
-                                self.output.warn('setting system attribute on "%s"' % filename)
-                                ctypes.windll.kernel32.SetFileAttributesW(filename, attributes | FILE_ATTRIBUTE_SYSTEM)
+                            #if FILE_ATTRIBUTE_SYSTEM != (attributes & FILE_ATTRIBUTE_SYSTEM):
+                            self.output.warn('setting system attribute on "%s"' % filename)
+                            ctypes.windll.kernel32.SetFileAttributesW(filename, attributes | FILE_ATTRIBUTE_SYSTEM)
                     except UnicodeDecodeError:
                         pass
 
