9e78f161946537136ec790b93f4f368a5bb34903
Jerry W <jerrywiltse@gmail.com>
2017-12-05 14:00:30 -0500

    change test_package to use path [skip ci]

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -6,15 +6,13 @@ import os
 
 
 class TestPackage(ConanFile):
+
     def test(self):
-        new_path = os.path.join(self.deps_env_info['cygwin_installer'].CYGWIN_BIN)
-        with tools.environment_append({'PATH': new_path}):
-            self.run(r'%CYGWIN_BIN%\bash -c "awk --help"')
-            self.run(r'%CYGWIN_BIN%\bash -c "/bin/awk --help"')
-            self.run(r'%CYGWIN_BIN%\bash -c "/usr/bin/awk --help"')
-            self.run(r'cat --version')
-            self.run(r'make --version')
-            self.run(r'pkg-config --version')
-            self.run(r'windres --version')
-            self.run(r'gcc --version')
-            self.run(r'g++ --version')
+        bash = tools.which("bash.exe")
+        
+        if bash:
+            self.output.info("using bash.exe from: " + bash)
+        else:
+            raise ConanException("No instance of bash.exe could be found on %PATH%")
+        
+        self.run('bash.exe -c ^"make --version^"')
