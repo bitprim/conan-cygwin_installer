be355c4c6d2fa11e859916892d1819c7df3d0093
Fernando Pelliccioni <fpelliccioni@gmail.com>
2018-01-25 17:29:11 -0300

    fix

--- build.py
+++ build.py
@@ -1,14 +1,32 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+# #!/usr/bin/env python
+# # -*- coding: utf-8 -*-
 
 
-from bincrafters import build_template_installer
-import os
+from conan.packager import ConanMultiPackager
 
 if __name__ == "__main__":
+    builder = ConanMultiPackager(username="bitprim", channel="stable", archs=["x86_64"])
+    # builder.add_common_builds(shared_option_name="bitprim-conan-boost:shared")
 
-    builder = build_template_installer.get_builder()
+    # filtered_builds = []
+    # for settings, options, env_vars, build_requires in builder.builds:
+    #     if (settings["build_type"] == "Release" or settings["build_type"] == "Debug") \
+    #             and not options["bitprim-conan-boost:shared"]:
 
-    builder.add({"arch": os.environ["CONAN_ARCHS"]}, {}, {}, {})
+    #         filtered_builds.append([settings, options, env_vars, build_requires])
 
+    # builder.builds = filtered_builds
     builder.run()
+
+
+
+# from bincrafters import build_template_installer
+# import os
+
+# if __name__ == "__main__":
+
+#     builder = build_template_installer.get_builder()
+
+#     builder.add({"arch": os.environ["CONAN_ARCHS"]}, {}, {}, {})
+
+#     builder.run()
