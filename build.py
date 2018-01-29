0fdac7f2a552166651376df40eccb931f71b3bcf
Fernando Pelliccioni <fpelliccioni@gmail.com>
2018-01-29 17:14:55 -0300

    fix

--- build.py
+++ build.py
@@ -11,9 +11,7 @@ if __name__ == "__main__":
 
     filtered_builds = []
     for settings, options, env_vars, build_requires in builder.builds:
-        if settings["build_type"] == "Release"
-            and (not "compiler.runtime" in settings or not settings["compiler.runtime"] == "MT"):
-        
+        if settings["build_type"] == "Release" and (not "compiler.runtime" in settings or not settings["compiler.runtime"] == "MT"):
             filtered_builds.append([settings, options, env_vars, build_requires])
 
     builder.builds = filtered_builds
